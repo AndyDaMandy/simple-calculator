b8a9cc4b05cc1aae60334111583fef242a66a444
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-04-17 12:13:56 -0400

    began building starting functions and html

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let container = document.querySelector('#container');
+let display = [];
 
+function showdisplay(){
+    container.innerHTML = '';
+   let displayer = display.join(' ');
+   let pusher = document.createElement('p');
+   pusher.textContent = displayer;
+   container.appendChild(pusher);
+}
+
+function addItem(char){
+    display.push(char)
+}
