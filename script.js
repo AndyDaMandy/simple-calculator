546964676c9204cce9818833b7eb22cd4d5ae326
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-04-19 08:50:10 -0400

    can now add items to the calculator

--- script.js
+++ script.js
@@ -10,5 +10,32 @@ function showdisplay(){
 }
 
 function addItem(char){
-    display.push(char)
+    let string = char.toString();
+    if (char !== '=' || char !== '/' || char !== '*' || char !== '+' || char !== '-'){
+        if (display.length > 0){
+            display[display.length -1] = display[display.length -1] += string;
+     // display.splice(-1, 1, string);
+        } else {
+            display.push(string);
+        }
+      
+    } else {
+        display.push(char);
+    }
+    showdisplay();
 }
+document.querySelector('#one').onclick = function(){addItem(1);}
+document.querySelector('#two').onclick = function(){addItem(2);}
+document.querySelector('#three').onclick = function(){addItem(3);}
+document.querySelector('#four').onclick = function(){addItem(4);}
+document.querySelector('#five').onclick = function(){addItem(5);}
+document.querySelector('#six').onclick = function(){addItem(6);}
+document.querySelector('#seven').onclick = function(){addItem(7);}
+document.querySelector('#eight').onclick = function(){addItem(8);}
+document.querySelector('#nine').onclick = function(){addItem(9);}
+document.querySelector('#zero').onclick = function(){addItem(0);}
+document.querySelector('#equal').onclick = function(){addItem('=');}
+document.querySelector('#plus').onclick = function(){addItem('+');}
+document.querySelector('#multiply').onclick = function(){addItem('*');}
+document.querySelector('#minus').onclick = function(){addItem('-');}
+document.querySelector('#divide').onclick = function(){addItem('/');}
